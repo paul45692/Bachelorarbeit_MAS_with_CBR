2a25a6e6a3c46e95e745709890c636b9930f9787
paul4 <paul4@LAPTOP-7VHBMS3F.fritz.box>
2018-11-24 15:15:26 +0100

    Logger angepasst

--- src/de/blanke/ba/logger/Test.java
+++ src/de/blanke/ba/logger/Test.java
@@ -1,9 +1,19 @@
 package de.blanke.ba.logger;
 
-import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
 public class Test {
 	
+	private static final Logger logger = Logger.getLogger(Test.class);
+	
+	public static void main(String[]args) {
+		PropertyConfigurator.configure(Test.class.getResource("log4j.info"));
+		System.out.println("Test");
+		
+		logger.info("Programm gestarted");
+		System.out.println("Testausgabe ->");
+		
+	}
 
 }
