b40d48b473749f8015ef4dc52ca39d4dc5947a9a
paul4 <paul4@147.172.162.190>
2018-12-04 16:32:45 +0100

    Eine einfache CBR Anbindung

--- src/de/blanke/ba/cbr/CBR_Engine.java
+++ src/de/blanke/ba/cbr/CBR_Engine.java
@@ -1,6 +1,14 @@
 package de.blanke.ba.cbr;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
+import de.dfki.mycbr.core.DefaultCaseBase;
 import de.dfki.mycbr.core.Project;
+import de.dfki.mycbr.core.casebase.Instance;
+import de.dfki.mycbr.core.model.Concept;
+import de.dfki.mycbr.core.model.SymbolDesc;
+import de.dfki.mycbr.core.retrieval.Retrieval;
 
 /**
  * Diese Klasse stellt die notwendigen Daten bereit,
@@ -10,18 +18,41 @@ import de.dfki.mycbr.core.Project;
  */
 public class CBR_Engine {
 	
-	private String data_path = "src\\res\\*";
-	private String projectName = "mühle.prj";
 	
-	public Project loadCBRProject() {
-			Project project = null;
-		try {
-			 project = new Project(data_path + projectName);
-		} catch (Exception e) {
-			System.out.println("Das Einlesen des Projektes ist fehlgeschlagen!");
-			e.printStackTrace();
-		}
-		return project;
+	
+	public Project createCBRProject() {
+			Project p = null;
+			Concept c = null;
+			
+			
+			try {
+				p = new Project();
+				c = p.createTopConcept("Mühle_Basis");
+				HashSet<String> data = new HashSet<String>();
+				String[] dataNames = {"A","B","C"};
+				data.addAll(Arrays.asList(dataNames));
+				SymbolDesc counter = new SymbolDesc(c,"Counter", data);
+				
+				DefaultCaseBase cb = p.createDefaultCB("myCaseBase");
+				Instance i = c.addInstance("Fall 1");
+				i.addAttribute(counter, "A");
+				cb.addCase(i);
+				System.out.print("Erfolgreich erstellt");
+				
+				Retrieval r = new Retrieval(c, cb);
+				Instance q = r.getQueryInstance();
+				q.addAttribute(counter.getName(),counter.getAttribute("A"));
+				r.start();
+				System.out.println("Abgeschickt: " + r.entrySet().size());
+				p.save();
+				
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		
+		return p;
 	}
 	
 }
