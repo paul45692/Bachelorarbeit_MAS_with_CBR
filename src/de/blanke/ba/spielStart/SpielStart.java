e45d204715ec450e27cf5f7e5097472043c0ccfb
paul4 <paul4@LAPTOP-7VHBMS3F.fritz.box>
2018-12-30 18:30:44 +0100

    Eine Quellenangabe eingefügt.

--- src/de/blanke/ba/spielStart/SpielStart.java
+++ src/de/blanke/ba/spielStart/SpielStart.java
@@ -3,6 +3,9 @@ package de.blanke.ba.spielStart;
 import de.blanke.ba.gui.GameLoop;
 /**
  * Diese Klasse stellt den Spieleinstieg bereit.
+ * Das Spielfeld, das aus dem res Ordner geladen wird, wurde von der
+ * folgenden Quelle übernommen (Stand 30.12.3018) 
+ * https://de.wikipedia.org/wiki/M%C3%BChle_(Spiel)#/media/File:Nine_Men%27s_Morris_board.svg
  * @author Paul Blanke.
  *
  */
